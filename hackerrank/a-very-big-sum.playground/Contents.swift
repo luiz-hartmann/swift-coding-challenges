d0af1949540f03d5cc26843557b29402fcbe750a
Luiz Hartmann <luiz-hartmann@outlook.com>
2022-04-23 11:21:55 -0400

    Hacker Rank - Challenge 02

---
+++ hackerrank/a-very-big-sum.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+func aVeryBigSum(ar: [Int]) -> Int {
+    return ar.reduce(0, +)
+}
+
+
+
+
